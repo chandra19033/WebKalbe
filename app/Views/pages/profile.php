362e6cd80132800ef72fe815ed38d3def9c90cfa
filbert29 <fkhouwira70@gmail.com>
2022-09-16 14:28:03 +0700

    coba profile

--- app/Views/pages/profile.php
+++ app/Views/pages/profile.php
@@ -1,1 +1,24 @@
-<h1>profile</h1>
+<style>
+    .left-side {
+        border-right: 1px solid white;
+
+    }
+
+    .profile {
+        height: 70vh;
+        background-image: linear-gradient(#428042, #3BB73B);
+    }
+</style>
+
+<section id="profile">
+    <div class="profile d-flex">
+        <div class="left-side col-4">
+            <div>
+                <img src="./../../../assets/profile-icon.png" alt="">
+            </div>
+        </div>
+        <div class="right-side col-8">
+            asdasd
+        </div>
+    </div>
+</section>
