bf15d511457e2e4202c0693154a0a601aec13382
chandra19033 <67153739+chandra19033@users.noreply.github.com>
2022-09-20 09:47:53 +0700

    Update footer.php

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -2,6 +2,11 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-</body>
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-image: linear-gradient(#428042, #3BB73B); color:white;">
+        © 2022 - PT Kalbe Farma Tbk
 
-</html>
+    </div>
+    <!-- Copyright -->
+</footer>
