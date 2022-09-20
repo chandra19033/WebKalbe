4452e0e15bddca7b80bb818cabe509feaff4200c
chandra19033 <67153739+chandra19033@users.noreply.github.com>
2022-09-20 11:55:50 +0700

    update

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,13 +1,12 @@
-<section class="vh-100">
-    <div class="container-fluid h-custom">
+<section class="vh-100" style=" background-image: url(assets/login.jpg); background-size: contain; background-repeat: no-repeat;" id="mainpage">
+    <div class="container-fluid h-custom ">
         <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-md-9 col-lg-6 col-xl-5">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image">
+            <div class="col-md-9 col-lg-6 col-xl-7">
             </div>
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                 <form>
-                    <div class="divider d-flex align-items-center my-4">
-                        <p class="text-center fw-bold mx-3 mb-0" style=" font-weight: 800; font-size: 32px;">Sign in</p>
+                    <div class="divider d-flex justify-content-center align-items-center my-5">
+                        <p class="text-center fw-bold mx-3 mb-0" style=" font-weight: 800; font-size: 60px;">Sign in</p>
                     </div>
 
                     <!-- Email input -->
@@ -33,7 +32,7 @@
                     </div>
 
                     <div class="text-center text-lg-start mt-4 pt-2">
-                        <button type="button" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                        <button type="button" class="btn btn-primary btn-lg" style="padding-left: 11rem; padding-right: 11rem;">Login</button>
                     </div>
 
                 </form>
